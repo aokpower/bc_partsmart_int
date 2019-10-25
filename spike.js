435f271669ace2bc92e0935b6ccf4e26d77db6eb
Cooper LeBrun <cooperlebrun@gmail.com>
2019-10-25 10:49:23 -0700

    move bc fnality into obj, add no cart case, cleanup

--- spike.js
+++ spike.js
@@ -1,51 +1,49 @@
-// Utility functions
-const sleep = ms => {
-    return new Promise(r => setTimeout(r, ms));  
+function MyBC(params) {
+	this.carts = () => {
+    const get_cart_path = '/api/storefront/carts?include=';
+    const base_options = {
+      'credentials': 'include',
+      'headers': {
+        'Accept':       'application/json',
+        'Content-Type': 'application/json'
+      }
+    };
+    return fetch(get_cart_path, base_options).then(r => { return r.json() });
+	}
+
+	this.addItem = (productId, quantity) => {
+    const payload = JSON.stringify({
+      'lineItems': [
+        {
+        	"productId": Number(productId),
+        	"quantity": Number(quantity)
+      	}
+    	]
+  	});
+
+		/* Why is get cartId logic not extracted to a separate fn?
+			 Because if there's no cart, you need an item to start
+			 a new one with. */
+    carts().then(cs => {
+      if (cs.length !== 0) { // if cart already exists, use it
+        const cart_endpoint = '/api/storefront/carts/' + cs[0].id + '/items';
+      } else {               // if cart doesn't exist, make a new one
+      	const cart_endpoint = '/api/storefront/cart';
+      }
+        return fetch(cart_endpoint, {
+          method: "POST",
+          credentials: "same-origin",
+          headers: { "Content-Type": "application/json" },
+          body: payload, 
+        });
+    }).then(res => { return res.json() });
+	}
 }
 
-const getCarts = () => {
-  const get_cart_path = '/api/storefront/carts?include=';
-  const base_options = {
-    'credentials': 'include',
-    'headers': {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  };
-  return fetch(get_cart_path, base_options)
-    .then(r => r.json());
-}
-
-// Note: line items should just be array of li objects
-/*
-TODO: Only .json() response if response code is good otherwise throw b/c:
+/* TODO: Only .json() response if response code is good otherwise throw b/c:
 currently, if response is a 504 with no body then it throws a JSON parser error
 */
-const addItemToCart = async(line_items) => {
-  // Note: Assumes first cart returned by getCart is target cart if one isn't
-  // provided
-  let cartId = await getCarts().then(carts => {
-    if (carts.length === 0) throw new Error("No carts available to post to.");
-    // TODO: Make cart if there isn't one already *IMPORTANT*
-    return carts[0].id;
-  });
-  // post line item to cart
-  const cart_endpoint = '/api/storefront/carts/' + cartId + '/items';
-  const payload = JSON.stringify({ 'lineItems': line_items });
-
-  const response = await fetch(cart_endpoint, {
-    method: "POST",
-    credentials: "same-origin",
-    headers: {
-      "Content-Type": "application/json" },
-    body: payload, 
-  });
-  return response.json()
-}
-
-// TODO: get exact parameter info for hook Fn
-const addToCartHook = (info) => {
-
-  // should be able to concurrently get ids for skus from the partsmart callback
-  // info
+const addToCartARI = (info) => {
+  // TODO: Run test with this to see exactly what parameter data looks like, and document here
+  console.log(info) 
 }
