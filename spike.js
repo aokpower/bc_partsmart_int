8bc74615604b1436f3aa33852fdcc54657aeb8eb
Cooper LeBrun <cooperlebrun@gmail.com>
2019-11-01 12:04:10 -0700

    Tabs -> Spaces, Start ARI hook fn, id lookup fn. Misc.

--- spike.js
+++ spike.js
@@ -1,5 +1,5 @@
 function MyBC(params) {
-	this.carts = () => {
+  this.carts = () => {
     const get_cart_path = '/api/storefront/carts?include=';
     const base_options = {
       'credentials': 'include',
@@ -8,22 +8,22 @@ function MyBC(params) {
         'Content-Type': 'application/json'
       }
     };
-    return fetch(get_cart_path, base_options).then(r => { return r.json() });
-	}
+    return fetch(get_cart_path, base_options).then(r => r.json());
+  }
 
-	this.addItem = (productId, quantity) => {
+  this.addItem = (productId, quantity) => {
     const payload = JSON.stringify({
       'lineItems': [
         {
-        	"productId": String(productId),
-        	"quantity": Number(quantity)
-      	}
-    	]
-  	});
+          "productId": String(productId),
+          "quantity": Number(quantity)
+        }
+      ]
+    });
 
-		/* Why is get cartId logic not extracted to a separate fn?
-			 Because if there's no cart, you need an item to start
-			 a new one with. */
+    /* Why is get cartId logic not extracted to a separate fn?
+       Because if there's no cart, you need an item to start
+       a new one with. */
     return this.carts().then(cs => {
       let cart_endpoint;
       if (cs.length !== 0) { // if cart already exists, use it
@@ -37,14 +37,37 @@ function MyBC(params) {
           headers: { "Content-Type": "application/json" },
           body: payload, 
         });
-    }).then(res => { return res.json() });
-	}
+    }).then(res => res.json());
+  }
 }
 
-/* TODO: Only .json() response if response code is good otherwise throw b/c:
-currently, if response is a 504 with no body then it throws a JSON parser error
-*/
-const addToCartARI = (info) => {
-  // TODO: Run test with this to see exactly what parameter data looks like, and document here
-  console.log(info) 
+const id_lookup = (ari_id) => {
+  /*
+    things that can happen from service:
+      - Service isn't reachable (failure state)
+      - Service returns Error   (failure state)
+      - No record found         (failure state)
+      - Record found with id    (Success state)
+  */
+  fetch
+}
+
+// errors if fn def form (const addToCartARI = ... is used, idk why)
+function addToCartARI(params_string) => {
+  // Convert string input into object
+  const params = params_string.split("&")
+    .map(param_string => param_string.split("="))
+    .reduce((obj, param_pair) => {
+      obj[param_pair[0]] = param_pair[1];
+      return obj;
+    }, {});
+
+  // Lookup sku to BC id from custom service
+  /*
+
+
+    Then need to pass that value to addToCartBC and handle possible states there
+
+  */
+
 }
