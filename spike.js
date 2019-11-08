39f79f880710a3908ffd97778b451af42d8005e2
Cooper LeBrun <cooperlebrun@gmail.com>
2019-11-08 15:22:14 -0800

    bunch of stuff. see extended notes
    - add utility functions: promiseTimeout and handleResponseError
    - migrate class like stuff to actual es 2015 class syntax
    - migrate some functions to async/await style syntax

--- spike.js
+++ spike.js
@@ -1,17 +1,38 @@
-function MyBC(params) {
-  this.carts = () => {
+// Utility:
+const promiseTimeout = (ms, promise) => {
+  let timeout = new Promise((resolve, reject) => {
+    let id = setTimeout(() => {
+      clearTimeout(id);
+      reject('Timed out in '+ms+' ms.');
+    }, ms);
+  });
+
+	// Do i need to return this?
+  Promise.race([promise, timeout]);
+}
+
+const handleResponseError = (response) => {
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return response;
+}
+
+// Integration:
+class MyBC {
+  carts() {
     const get_cart_path = '/api/storefront/carts?include=';
     const base_options = {
       'credentials': 'include',
       'headers': {
-        'Accept':       'application/json',
+        'Accept': 'application/json',
         'Content-Type': 'application/json'
       }
     };
     return fetch(get_cart_path, base_options).then(r => r.json());
-  }
+  };
 
-  this.addItem = (productId, quantity) => {
+  async addItem(productId, quantity) {
     const payload = JSON.stringify({
       'lineItems': [
         {
@@ -20,54 +41,61 @@ function MyBC(params) {
         }
       ]
     });
-
     /* Why is get cartId logic not extracted to a separate fn?
-       Because if there's no cart, you need an item to start
-       a new one with. */
-    return this.carts().then(cs => {
-      let cart_endpoint;
-      if (cs.length !== 0) { // if cart already exists, use it
-        cart_endpoint = '/api/storefront/carts/' + cs[0].id + '/items';
-      } else {               // if cart doesn't exist, make a new one
-        cart_endpoint = '/api/storefront/cart';
-      }
-        return fetch(cart_endpoint, {
-          method: "POST",
-          credentials: "same-origin",
-          headers: { "Content-Type": "application/json" },
-          body: payload, 
-        });
-    }).then(res => res.json());
+        Because if there's no cart, you need an item to start
+        a new one with. */
+    const cs = await this.carts();
+    let cart_endpoint;
+    if (cs.length !== 0) { // if cart already exists, use it
+      cart_endpoint = '/api/storefront/carts/' + cs[0].id + '/items';
+    }
+    else { // if cart doesn't exist, make a new one
+      cart_endpoint = '/api/storefront/cart';
+    }
+
+    const res = await fetch(cart_endpoint, {
+      method: "POST",
+      credentials: "same-origin",
+      headers: { "Content-Type": "application/json" },
+      body: payload,
+    });
+    // TODO: if res.ok { return await res.json() } else { promise.reject(err)? }
+    return await res.json();
+  };
+}
+
+class IdLookup {
+  constructor(config) {
+    this.config = config;
+  }
+
+  check(ari_sku) {
+    return new Promise((resolve, reject) => {
+      fetch(this.config.host, this.config.options)
+        .then(handleResponseError)
+        // .then(...
+        // .catch(err => ...
+    });
   }
 }
 
-const id_lookup = (ari_id) => {
-  /*
-    things that can happen from service:
-      - Service isn't reachable (failure state)
-      - Service returns Error   (failure state)
-      - No record found         (failure state)
-      - Record found with id    (Success state)
-  */
-  fetch
-}
 
-// errors if fn def form (const addToCartARI = ... is used, idk why)
-function addToCartARI(params_string) => {
+// Main
+var bc = new MyBC();
+
+// errors if => fn form is used
+function addToCartARI(params_str) {
   // Convert string input into object
-  const params = params_string.split("&")
-    .map(param_string => param_string.split("="))
+  const params = params_str.split("&")
+    .map(param_str => param_str.split("="))
     .reduce((obj, param_pair) => {
       obj[param_pair[0]] = param_pair[1];
       return obj;
     }, {});
 
+  id_lookup(params.arisku).then(id => {
+    bc.addItem(id)
+  }).catch(err => console.log(err.message))
+
   // Lookup sku to BC id from custom service
-  /*
-
-
-    Then need to pass that value to addToCartBC and handle possible states there
-
-  */
-
 }
